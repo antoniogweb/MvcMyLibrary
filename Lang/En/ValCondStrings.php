27fd6ec6b981ad09a0bc1d865f42bd7bfb26d931
antonio <antoniog.web@gmail.com>
2024-06-28 15:01:21 +0200

    translation

--- Lang/En/ValCondStrings.php
+++ Lang/En/ValCondStrings.php
@@ -38,82 +38,90 @@ class Lang_En_ValCondStrings {
 		return $html;
 	}
 	
+	public function translateElement($element)
+	{
+		if (Params::$translatorFunction)
+			$element = call_user_func(Params::$translatorFunction, $element);
+				
+		return $element;
+	}
+	
 	//if the element is not defined
 	public function getNotDefinedResultString($element)
 	{
-		return "<div class='".Params::$errorStringClassName."'>Please fill the field <i>". getFieldLabel($element) ."</i>.</div>\n".$this->getHiddenAlertElement($element);
+		return "<div class='".Params::$errorStringClassName."'>Please fill the field <i>". $this->translateElement(getFieldLabel($element)) ."</i>.</div>\n".$this->getHiddenAlertElement($element);
 	}
 	
 	//if the elements are not equal
 	public function getNotEqualResultString($element)
 	{
-		return "<div class='".Params::$errorStringClassName."'>Please check that the fields <i>".getFieldLabel($element)."</i> are equal</div>\n".$this->getHiddenAlertElement($element);
+		return "<div class='".Params::$errorStringClassName."'>Please check that the fields <i>".$this->translateElement(getFieldLabel($element))."</i> are equal</div>\n".$this->getHiddenAlertElement($element);
 	}
 	
 	//if the element is not alphabetic
 	public function getNotAlphabeticResultString($element)
 	{
-		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".getFieldLabel($element)."</i> is alphabetic</div>\n".$this->getHiddenAlertElement($element);
+		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".$this->translateElement(getFieldLabel($element))."</i> is alphabetic</div>\n".$this->getHiddenAlertElement($element);
 	}
 
 	//if the element is not alphanumeric
 	public function getNotAlphanumericResultString($element)
 	{
-		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".getFieldLabel($element)."</i> has to be alphanumeric</div>\n".$this->getHiddenAlertElement($element);
+		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".$this->translateElement(getFieldLabel($element))."</i> has to be alphanumeric</div>\n".$this->getHiddenAlertElement($element);
 	}
 	
 	//if the element is not a decimal digit
 	public function getNotDecimalDigitResultString($element)
 	{
-		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".getFieldLabel($element)."</i> is a decimal digit</div>\n".$this->getHiddenAlertElement($element);
+		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".$this->translateElement(getFieldLabel($element))."</i> is a decimal digit</div>\n".$this->getHiddenAlertElement($element);
 	}
 	
 	//if the element hasn't the mail format
 	public function getNotMailFormatResultString($element)
 	{
-		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".getFieldLabel($element)."</i> is an e-mail address</div>\n".$this->getHiddenAlertElement($element);
+		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".$this->translateElement(getFieldLabel($element))."</i> is an e-mail address</div>\n".$this->getHiddenAlertElement($element);
 	}
 
 	//if the element is not numeric
 	public function getNotNumericResultString($element)
 	{
-		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".getFieldLabel($element)."</i> is numeric</div>\n".$this->getHiddenAlertElement($element);
+		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".$this->translateElement(getFieldLabel($element))."</i> is numeric</div>\n".$this->getHiddenAlertElement($element);
 	}
 	
 	//if the element is not an integer
 	public function getNotIntegerFormatResultString($element)
 	{
-		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".getFieldLabel($element)."</i> is an integer number</div>\n".$this->getHiddenAlertElement($element);
+		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".$this->translateElement(getFieldLabel($element))."</i> is an integer number</div>\n".$this->getHiddenAlertElement($element);
 	}
 	
 	//if the element is not a real date
 	public function getNotDateResultString($element)
 	{
-		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".getFieldLabel($element)."</i> is a real date</div>\n".$this->getHiddenAlertElement($element);
+		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".$this->translateElement(getFieldLabel($element))."</i> is a real date</div>\n".$this->getHiddenAlertElement($element);
 	}
 	
 	//if the element (string) length exceeds the value of characters (defined by $maxLength)
 	public function getLengthExceedsResultString($element,$maxLength)
 	{
-		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".getFieldLabel($element)."</i> does not exceed the value of $maxLength characters</div>\n".$this->getHiddenAlertElement($element);
+		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".$this->translateElement(getFieldLabel($element))."</i> does not exceed the value of $maxLength characters</div>\n".$this->getHiddenAlertElement($element);
 	}
 
 	//if the element is one of the strings indicated by $stringList (a comma-separated list of strings)
 	public function getIsForbiddenStringResultString($element,$stringList)
 	{
-		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".getFieldLabel($element)."</i> is not one of the following strings: $stringList</div>\n".$this->getHiddenAlertElement($element);
+		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".$this->translateElement(getFieldLabel($element))."</i> is not one of the following strings: $stringList</div>\n".$this->getHiddenAlertElement($element);
 	}
 
 	//if the element is not one of the strings indicated by $stringList (a comma-separated list of strings)
 	public function getIsNotStringResultString($element,$stringList)
 	{
-		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".getFieldLabel($element)."</i> is one of the following strings: $stringList</div>\n".$this->getHiddenAlertElement($element);
+		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".$this->translateElement(getFieldLabel($element))."</i> is one of the following strings: $stringList</div>\n".$this->getHiddenAlertElement($element);
 	}
 
 	//if the element does not match the reg expr indicated by $regExp
 	public function getDoesntMatchResultString($element,$regExp)
 	{
-		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".getFieldLabel($element)."</i> matchs the following regular expression: $regExp</div>\n".$this->getHiddenAlertElement($element);
+		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".$this->translateElement(getFieldLabel($element))."</i> matchs the following regular expression: $regExp</div>\n".$this->getHiddenAlertElement($element);
 	}
 	
 	//if the element is not decimal
@@ -123,7 +131,7 @@ class Lang_En_ValCondStrings {
 		$M = (int)$t[0];
 		$D = (int)$t[1];
 		$I = $M - $D;
-		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".getFieldLabel($element)."</i> is a decimal number (maximum number of integer digits:$I, maximum number of decimal digits: $D)</div>\n".$this->getHiddenAlertElement($element);
+		return "<div class='".Params::$errorStringClassName."'>Please check that the field <i>".$this->translateElement(getFieldLabel($element))."</i> is a decimal number (maximum number of integer digits:$I, maximum number of decimal digits: $D)</div>\n".$this->getHiddenAlertElement($element);
 	}
 	
 }

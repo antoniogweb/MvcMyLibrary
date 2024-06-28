3acab79b8e3d4acf337905e88b8547e2017cb18d
antonio <antoniog.web@gmail.com>
2024-06-28 14:26:31 +0200

    translation

--- Lang/En/Formats/Fields.php
+++ Lang/En/Formats/Fields.php
@@ -39,6 +39,10 @@ class Lang_En_Formats_Fields
 		else
 		{
 			$fieldName = str_replace("_"," ", $fieldName);
+			
+			if (Params::$translatorFunction)
+				$fieldName = call_user_func(Params::$translatorFunction, $fieldName);
+			
 			return ucfirst($fieldName);
 		}
 	}
